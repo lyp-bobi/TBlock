535e04bdf047d273e8afc4f43d8b36bb6b1f533e
lyp-bobi <34655025+lyp-bobi@users.noreply.github.com>
2023-01-28 14:54:30 +0800

    Update Bound.c

--- Bound.c
+++ Bound.c
@@ -7,7 +7,6 @@
 #include <string.h>
 #include <float.h>
 
-#define SIZE_BLOCKLIST_PADDING (4) // for pg varsize
 #define SIZE_BLOCKLIST_FLAG (1)
 
 char *boolsToBytes(bool *t, int numbox) {
@@ -38,10 +37,10 @@ bool *bytesToBools(char *b, int numbox) {
 unsigned int boundlist_datalen_2d(BOUNDLISTTYPE type, unsigned int numbox) {
     switch (type) {
         case BLT_boxlist:
-            return SIZE_BLOCKLIST_PADDING + SIZE_BLOCKLIST_FLAG +
+            return SIZE_BLOCKLIST_FLAG +
                    numbox * 4 * sizeof(float);
         case BLT_blocklist:
-            return SIZE_BLOCKLIST_PADDING + SIZE_BLOCKLIST_FLAG +
+            return SIZE_BLOCKLIST_FLAG +
                    ((numbox + 7) / 8) + (numbox + 1) * (2 * sizeof(float));
     }
 }
@@ -49,11 +48,11 @@ unsigned int boundlist_datalen_2d(BOUNDLISTTYPE type, unsigned int numbox) {
 unsigned int boundlist_numbox_2d(BOUNDLISTTYPE type, unsigned int datalen) {
     switch (type) {
         case BLT_boxlist:
-            return (datalen - SIZE_BLOCKLIST_PADDING - SIZE_BLOCKLIST_FLAG) /
+            return (datalen - SIZE_BLOCKLIST_FLAG) /
                    4 / sizeof(float);
         case BLT_blocklist:
             return (int) (
-                    (datalen - SIZE_BLOCKLIST_PADDING - SIZE_BLOCKLIST_FLAG) /
+                    (datalen - SIZE_BLOCKLIST_FLAG) /
                     (1.0 / 8 + 2 * sizeof(float))) - 1;
     }
 }
@@ -63,11 +62,10 @@ BOUNDLIST_SERL boundlist_serl_2d(BOUNDLIST *bl) {
     buf.BLB_size = boundlist_datalen_2d(bl->BL_type, bl->BL_numbox);
     char *res = malloc(buf.BLB_size);
     buf.BLB_data = res;
-    res[SIZE_BLOCKLIST_PADDING] = bl->BL_type;
+    res[0] = bl->BL_type;
     switch (bl->BL_type) {
         case BLT_boxlist: {
-            float *fltdata = (float *) &res[SIZE_BLOCKLIST_PADDING +
-                                            SIZE_BLOCKLIST_FLAG];
+            float *fltdata = (float *) &res[SIZE_BLOCKLIST_FLAG];
             int cur = 0;
             for (int i = 0; i < bl->BL_numbox; i++) {
                 fltdata[cur++] = bl->BL_data[i].B_data[0];
@@ -80,8 +78,7 @@ BOUNDLIST_SERL boundlist_serl_2d(BOUNDLIST *bl) {
         case BLT_blocklist: {
             int len_flag = (bl->BL_numbox + 7) / 8;
             bool *types = (bool *) malloc(sizeof(bool) * bl->BL_numbox);
-            float *fltdata = (float *) (res + SIZE_BLOCKLIST_PADDING +
-                                        SIZE_BLOCKLIST_FLAG + len_flag);
+            float *fltdata = (float *) (res + SIZE_BLOCKLIST_FLAG + len_flag);
             int cur = 0;
             for (int i = 0; i < bl->BL_numbox; i++) {
                 types[i] = bl->BL_data[i].B_type - BT_block1;
@@ -91,7 +88,7 @@ BOUNDLIST_SERL boundlist_serl_2d(BOUNDLIST *bl) {
             fltdata[cur++] = bl->BL_data[bl->BL_numbox - 1].B_data[2];
             fltdata[cur++] = bl->BL_data[bl->BL_numbox - 1].B_data[3];
             char *type_char = boolsToBytes(types, bl->BL_numbox);
-            memcpy(res+SIZE_BLOCKLIST_PADDING+ SIZE_BLOCKLIST_FLAG, type_char, len_flag);
+            memcpy(res + SIZE_BLOCKLIST_FLAG, type_char, len_flag);
         }
         break;
     }
@@ -101,7 +98,7 @@ BOUNDLIST_SERL boundlist_serl_2d(BOUNDLIST *bl) {
 BOUNDLIST *boundlist_deserl_2d(BOUNDLIST_SERL in) {
     char *data = in.BLB_data;
     unsigned int datalen = in.BLB_size;
-    BOUNDLISTTYPE type = data[SIZE_BLOCKLIST_PADDING];
+    BOUNDLISTTYPE type = data[0];
     int numbox = boundlist_numbox_2d(type, datalen);
     BOUNDLIST *bl = malloc(sizeof(BOUNDLIST) + sizeof(BOUND) * numbox);
     bl->BL_type = type;
@@ -111,7 +108,7 @@ BOUNDLIST *boundlist_deserl_2d(BOUNDLIST_SERL in) {
     switch (type) {
         case BLT_boxlist: {
             float *fltdata = (float*)(
-                    data + SIZE_BLOCKLIST_PADDING + SIZE_BLOCKLIST_FLAG);
+                    data + SIZE_BLOCKLIST_FLAG);
             int cur = 0;
             for (int i = 0; i < numbox; i++) {
                 bl->BL_data[i].B_data[0] = fltdata[cur++];
@@ -123,14 +120,14 @@ BOUNDLIST *boundlist_deserl_2d(BOUNDLIST_SERL in) {
                 {
                     bl->xmin = bl->BL_data[i].B_data[0];
                 }
-                if(bl->xmax < bl->BL_data[i].B_data[1])
+                if(bl->ymin > bl->BL_data[i].B_data[1])
                 {
-                    bl->xmax = bl->BL_data[i].B_data[1];
+                    bl->ymin = bl->BL_data[i].B_data[1];
                 }
-                if(bl->ymin > bl->BL_data[i].B_data[2])
-                {
-                    bl->ymin = bl->BL_data[i].B_data[2];
-                }
+	            if(bl->xmax < bl->BL_data[i].B_data[2])
+	            {
+		            bl->xmax = bl->BL_data[i].B_data[2];
+	            }
                 if(bl->ymax < bl->BL_data[i].B_data[3])
                 {
                     bl->ymax = bl->BL_data[i].B_data[3];
@@ -141,9 +138,9 @@ BOUNDLIST *boundlist_deserl_2d(BOUNDLIST_SERL in) {
         case BLT_blocklist: {
             int len_flag = (numbox + 7) / 8;
             bool *types = bytesToBools(
-                    (data + SIZE_BLOCKLIST_PADDING + SIZE_BLOCKLIST_FLAG),
+                    (data + SIZE_BLOCKLIST_FLAG),
                     numbox);
-            float *fltdata = (float*)(data + SIZE_BLOCKLIST_PADDING + SIZE_BLOCKLIST_FLAG + len_flag);
+            float *fltdata = (float*)(data + SIZE_BLOCKLIST_FLAG + len_flag);
             int cur = 0;
             for (int i = 0; i < bl->BL_numbox; i++) {
                 bl->BL_data[i].B_type = types[i] + BT_block1;
