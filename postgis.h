d4acac77652e806fe7c8a0199f6230eccfc0144b
lyp-bobi <34655025+lyp-bobi@users.noreply.github.com>
2023-01-28 14:54:00 +0800

    Create postgis.h

--- postgis.h
+++ postgis.h
@@ -4,6 +4,19 @@
 
 #ifndef TBLOCK_POSTGIS_H
 #define TBLOCK_POSTGIS_H
+#define LWFLAG_Z        0x01
+#define LWFLAG_M        0x02
+#define LWFLAG_BBOX     0x04
+#define LWFLAG_GEODETIC 0x08
+#define LWFLAG_READONLY 0x10
+#define LWFLAG_SOLID    0x20
+#define FLAGS_GET_Z(flags)         ((flags) & LWFLAG_Z)
+#define FLAGS_GET_M(flags)        (((flags) & LWFLAG_M)>>1)
+#define FLAGS_GET_BBOX(flags)     (((flags) & LWFLAG_BBOX)>>2)
+#define FLAGS_GET_GEODETIC(flags) (((flags) & LWFLAG_GEODETIC)>>3)
+#define FLAGS_GET_READONLY(flags) (((flags) & LWFLAG_READONLY)>>4)
+#define FLAGS_GET_SOLID(flags)    (((flags) & LWFLAG_SOLID)>>5)
+#define FLAGS_NDIMS(flags) (2 + FLAGS_GET_Z(flags) + FLAGS_GET_M(flags))
 /*  POINTARRAY
         *  Point array abstracts a lot of the complexity of points and point lists.
 *  It handles 2d/3d translation
